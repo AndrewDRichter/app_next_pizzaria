5f3901cafcc425b25b73235dc22876b48b4fffae
AndrewDRichter <andrewrichter1@hotmail.com>
2025-07-07 17:22:24 -0300

    signin, signup design finished, now implementing the functionalities

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,51 @@
 import styles from './page.module.scss';
-import logoImg from '/public/logo.svg'
+import jakaruImg from '/public/logo_ok.svg'
 import Image from 'next/image';
+import Link from 'next/link';
 
 export default function Page() {
   return (
     <>
       <div className={styles.containerCenter}>
         <Image
-          src={logoImg}
+          src={jakaruImg}
           alt='Company logo'
+          width={300}
+          priority
         />
+
+        <section className={styles.login}>
+          <form>
+            <input
+              type='email'
+              required
+              name='email'
+              placeholder='Ingresa tu email'
+              className={styles.input}
+            />
+
+            <input
+              type='password'
+              required
+              name='password'
+              placeholder='********'
+              className={styles.input}
+            />
+
+            <button
+              type='submit'
+            >
+              Entrar
+            </button>
+          </form>
+
+          <Link
+            href='/signup'
+            className={styles.text}
+          >
+            No tienes una cuenta? Regístrate
+          </Link>
+        </section>
       </div>
     </>
   )
---
+++ src/app/signup/page.tsx
@@ -0,0 +1,88 @@
+import { api } from '@/services/api';
+import styles from '../page.module.scss';
+import jakaruImg from '/public/logo_ok.svg'
+import Image from "next/image"
+import Link from "next/link"
+import { redirect } from 'next/navigation';
+
+export default function Signup() {
+
+    async function handleSignup(formData: FormData) {
+        'use server'
+
+        const name = formData.get('name')
+        const email = formData.get('email')
+        const password = formData.get('password')
+
+        if (name === '' || email === '' || password === '') {
+            return;
+        }
+
+        try {
+            await api.post("/users", {
+                name,
+                email,
+                password
+            })
+            redirect("/")
+        } catch (err) {
+            console.log(`Error: ${err}`)
+        }
+    }
+
+    return (
+        <>
+            <div className={styles.containerCenter}>
+                <Image
+                    src={jakaruImg}
+                    alt='Company logo'
+                    width={300}
+                    priority
+                />
+
+                <section className={styles.login}>
+
+                    <h1>Vamos a crear tu cuenta</h1>
+                    <form action={handleSignup}>
+                        <input
+                            type='text'
+                            required
+                            name='name'
+                            placeholder='Ingresa tu nombre'
+                            className={styles.input}
+                        />
+
+                        <input
+                            type='email'
+                            required
+                            name='email'
+                            placeholder='Ingresa tu email'
+                            className={styles.input}
+                        />
+
+                        <input
+                            type='password'
+                            required
+                            name='password'
+                            placeholder='********'
+                            className={styles.input}
+                        />
+
+                        <button
+                            type='submit'
+                        >
+                            Crear
+                        </button>
+                    </form>
+
+                    <Link
+                        href='/'
+                        className={styles.text}
+                    >
+                        Ya tienes una cuenta? Ingresá
+                    </Link>
+                </section>
+            </div>
+        </>
+    )
+}
