2e6b86c120badca5a02972a908503c98b3e3a22f
Andrew Richter <andrewrichter1@hotmail.com>
2025-07-11 22:11:01 -0300

    finished styling the orders listing page

--- src/app/dashboard/components/orders/index.tsx
+++ src/app/dashboard/components/orders/index.tsx
@@ -3,14 +3,27 @@ import { RefreshCwIcon } from 'lucide-react';
 
 export function Orders() {
     return (
-        <main className={styles.ordersContainer}>
+        <main className={styles.container}>
 
             <section className={styles.containerHeader}>
                 <h1>Latest orders</h1>
                 <button>
                     <RefreshCwIcon
-                        size={30}
+                        size={24}
+                        color='#3fffa3'
                     />
+                </button>
+            </section>
+
+            <section className={styles.listOrders}>
+                <button className={styles.orderItem}>
+                    <div className={styles.tag}></div>
+                    <span>Mesa 10</span>
+                </button>
+
+                <button className={styles.orderItem}>
+                    <div className={styles.tag}></div>
+                    <span>Mesa 13</span>
                 </button>
             </section>
 
